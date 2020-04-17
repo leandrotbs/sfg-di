4b4c2b490d2e840bbdf97dbbc23573f588bc6170
Leandro <Leandro@192.168.0.27>
2020-04-17 17:03:36 -0400

    Default Profile

--- src/main/java/guru/springframework/sfgdi/service/I18nSpanishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/service/I18nSpanishGreetingService.java
@@ -3,9 +3,15 @@ package guru.springframework.sfgdi.service;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES") 	// @Profile indicates which profile this bean belongs to. It will be injected
-				// only if the profile specified has been set as active. By using @Profiler you
-				// can have multiple beans with the same name, but only one active in runtime
+@Profile({ "ES", "default" }) 	/*  @Profile indicates which profile this bean belongs to. It will be injected
+								 *  only if the profile specified has been set as active. By using @Profiler you
+								 *  can have multiple beans with the same name, but only one active in runtime.
+								 *  
+								 *  A list of profile names can be provide by using curly braces {}
+								 *  
+								 *  In order to set a default profile, one of the profiles names has to be "default"
+								 */ 
+								
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService {
 
