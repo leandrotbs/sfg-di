de3508783d257482eb1a3cea5e3b81a98d47f817
Leandro <Leandro@192.168.0.27>
2020-04-17 16:40:27 -0400

    Adding SfgDiApplication that mistakenly had been ignored

---
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -0,0 +1,51 @@
+package guru.springframework.sfgdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
+import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.controllers.PropertyInjectedController;
+import guru.springframework.sfgdi.controllers.SetterInjectedController;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+		var ctx = SpringApplication.run(SfgDiApplication.class, args);
+		
+		var myController = (MyController) ctx.getBean("myController");
+		
+		System.out.println();
+		System.out.println("############ Primary bean");
+		System.out.println(myController.sayHello());
+		
+		
+		
+		System.out.println();
+		System.out.println("############ Property");
+		
+		var propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		
+		System.out.println(propertyInjectedController.getGreeting());
+		
+		
+		
+		System.out.println();
+		System.out.println("############ Setter");
+		
+		var setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		
+		System.out.println(setterInjectedController.getGreeting());
+		
+	
+		
+		System.out.println();
+		System.out.println("############ Constructor");
+		
+		var constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		
+		System.out.println(constructorInjectedController.getGreeting());
+	}
+
+}
